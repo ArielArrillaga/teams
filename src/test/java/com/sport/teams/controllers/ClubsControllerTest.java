773233504f9621f54275055fa7b9264564b8a4c9
Ariel Arrillaga <arielarrillagagm@gmail.com>
2024-08-11 21:31:59 -0300

    Se agregaron pruebas unitarias para ClubsController

--- src/test/java/com/sport/teams/controllers/ClubsControllerTest.java
+++ src/test/java/com/sport/teams/controllers/ClubsControllerTest.java
@@ -1,7 +1,9 @@
 package com.sport.teams.controllers;
 
 import com.sport.teams.entitys.clubs.Club;
+import com.sport.teams.entitys.clubs.ClubRequest;
 import com.sport.teams.services.ClubsService;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -28,15 +30,54 @@ public class ClubsControllerTest {
  public ClubsControllerTest() {
      MockitoAnnotations.openMocks(this);
  }
-
- @Test
- public void testGetClubByNombre() {
-     // Arrange
+ 
+  private List<Club> mockList() {
      Club club1 = new Club();
      club1.setNombre("FC Barcelona");
      Club club2 = new Club();
      club2.setNombre("Real Madrid");
-     List<Club> clubs = Arrays.asList(club1, club2);
+    return Arrays.asList(club1, club2);
+ }
+  
+  private Club mockClub() {
+	  Club club = new Club();
+      club.setNombre("FC Barcelona");
+      return club;
+  }
+  
+  @Test
+  void testFindAll() {
+	  // Arrange
+	  List<Club> clubs = this.mockList();
+      when(clubsService.findAll()).thenReturn(clubs);
+      
+	  //Act
+      ResponseEntity<List<Club>> response = clubsController.findAll();
+      
+      //Assert
+      assertEquals(HttpStatus.OK, response.getStatusCode());
+      assertEquals(2, response.getBody().size());
+      assertEquals(clubs, response.getBody());
+  }
+  
+  @Test
+  public void testGetClubById() {
+      // Arrange
+      Club club = this.mockClub();
+      when(clubsService.findById(1L)).thenReturn(club);
+
+      // Act
+      ResponseEntity<Club> response = clubsController.getClubById(1L);
+
+      // Assert
+      assertEquals(HttpStatus.OK, response.getStatusCode());
+      assertEquals(club, response.getBody());
+  }
+
+  @Test
+  public void testGetClubByNombre() {
+     // Arrange
+     List<Club> clubs = this.mockList();
      when(clubsService.findByNombre(anyString())).thenReturn(clubs);
 
      // Act
@@ -45,6 +86,49 @@ public class ClubsControllerTest {
      // Assert
      assertEquals(HttpStatus.OK, response.getStatusCode());
      assertEquals(clubs, response.getBody());
- }
+  }
+  
+  @Test
+  void testCreateClub() {
+      // Arrange
+      ClubRequest clubRequest = new ClubRequest();
+      clubRequest.setNombre("FC Barcelona");
+      Club club = new Club(clubRequest);
+      when(clubsService.createClub(clubRequest)).thenReturn(club);
+
+      // Act
+      ResponseEntity<Club> response = clubsController.createClub(clubRequest);
+
+      // Assert
+      assertEquals(HttpStatus.CREATED, response.getStatusCode());
+      assertEquals(club, response.getBody());
+  }
+  
+  @Test
+  void testUpdateClub() {
+      // Arrange
+	  ClubRequest clubRequest = new ClubRequest();
+	  clubRequest.setNombre("FC Barcelona");
+	  Club updatedClub = new Club();
+	  updatedClub.setNombre("FC Barcelona");
+      when(clubsService.updateClub(1L, clubRequest)).thenReturn(updatedClub);
+
+      // Act
+      ResponseEntity<Club> response = clubsController.updateClub(1L, clubRequest);
+
+      // Assert
+      assertEquals(HttpStatus.OK, response.getStatusCode());
+      assertEquals(updatedClub, response.getBody());
+  }
+  
+  @Test
+  void testDeleteClub() {
+      // Act
+      ResponseEntity<Void> response = clubsController.deleteClub(1L);
+
+      // Assert
+      assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+  }
+
 }
 
